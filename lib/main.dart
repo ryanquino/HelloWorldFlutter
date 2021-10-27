03059e311bee8fab903b0baa1144236ea43004ed
ryanquino <ryanquino@gmail.com>
2021-10-27 15:49:43 +0800

    october 27, 2021

--- lib/main.dart
+++ lib/main.dart
@@ -10,29 +10,69 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-            backgroundColor: Colors.deepOrangeAccent,
+            backgroundColor: Colors.teal,
             body: SafeArea(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Container(
-                  width: 100,
-                  height: 100,
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/me.png'),
+                ),
+                Text(
+                  'Ryan Quino',
+                  style: TextStyle(
+                      fontFamily: 'DancingScript',
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 20,
+                      color: Colors.teal.shade100,
+                      letterSpacing: 8,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
                   color: Colors.white,
-                  child: Text('I am a child of the container',
-                      style: TextStyle(fontSize: 16)),
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text('+63 912 3456 789',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                        )),
+                  ),
                 ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue,
-                  child: Text('I am a child of the container'),
-                ),
-                Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.red,
-                  child: Text('I am a child of the container'),
+                Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    title: Text('ryanquino@gmail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          fontSize: 20,
+                          color: Colors.teal.shade900,
+                        )),
+                  ),
                 ),
               ],
             ))));
